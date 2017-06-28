93c03c6a6c097f6a1275ca008bbe6e26c48cc5a1
Alex <fourthwall96@gmail.com>
2017-06-28 14:22:48 +0100

    Added Shaake Ad to list

--- scripts.js
+++ scripts.js
@@ -2,7 +2,8 @@
 function selectAd() {
     var ads = [
       ["bowlingad.svg", "Hollywood Bowl - Let the good times roll!", "http://www.hollywoodbowl.co.uk/"],
-      ["gbkad.svg", "Gourmet Burger Kitchen - Just Good Food", "https://www.gbk.co.uk/"]
+      ["gbkad.svg", "Gourmet Burger Kitchen - Just Good Food", "https://www.gbk.co.uk/"],
+      ["shaakead.svg", "Shaake - Fresh Juices Smoothies Milkshakes & More!", "https://www.shaake.com"]
     ];
 
     var adid = Math.floor(Math.random() * ads.length);
