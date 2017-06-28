365a31c0699c29ce2ee245e034d122b556b8ab3d
Cameron Cawley <ccawley2011@gmail.com>
2017-06-28 12:01:29 +0100

    Add random advert selection

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+function selectAd() {
+    var ads = [
+      ["bowlingad.svg", "Hollywood Bowl - Let the good times roll!", "http://www.hollywoodbowl.co.uk/"]
+    ];
+
+    var adid = Math.floor(Math.random() * ads.length);
+    document.getElementById("advert").src = "images/"+ads[adid][0];
+    document.getElementById("advert").alt = ads[adid][1];
+    document.getElementById("adlink").href = ads[adid][2];
+}
