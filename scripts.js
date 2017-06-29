8f408bc4df0024be7a4efb61df12035a357f96b5
Alex <fourthwall96@gmail.com>
2017-06-29 10:39:57 +0100

    Added Norwich Castle to banner rotation

--- scripts.js
+++ scripts.js
@@ -3,7 +3,8 @@ function selectAd() {
     var ads = [
       ["bowlingad.svg", "Hollywood Bowl - Let the good times roll!", "http://www.hollywoodbowl.co.uk/"],
       ["gbkad.svg", "Gourmet Burger Kitchen - Just Good Food", "https://www.gbk.co.uk/"],
-      ["shaakead.svg", "Shaake - Fresh Juices Smoothies Milkshakes & More!", "https://www.shaake.com"]
+      ["shaakead.svg", "Shaake - Fresh Juices Smoothies Milkshakes & More!", "https://www.shaake.com"],
+      ["norwichcastle.svg", "Norwich Castle - Museum & Art Gallery - 900 years of history", "http://www.museums.norfolk.gov.uk/Visit_Us/Norwich_Castle/index.htm"]
     ];
 
     var adid = Math.floor(Math.random() * ads.length);
