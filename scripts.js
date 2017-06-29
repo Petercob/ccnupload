c71221ba32c64aacfb7a2221e8b5c31b64e90f72
Alex <fourthwall96@gmail.com>
2017-06-29 10:40:59 +0100

    Changed svg name

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ function selectAd() {
       ["bowlingad.svg", "Hollywood Bowl - Let the good times roll!", "http://www.hollywoodbowl.co.uk/"],
       ["gbkad.svg", "Gourmet Burger Kitchen - Just Good Food", "https://www.gbk.co.uk/"],
       ["shaakead.svg", "Shaake - Fresh Juices Smoothies Milkshakes & More!", "https://www.shaake.com"],
-      ["norwichcastle.svg", "Norwich Castle - Museum & Art Gallery - 900 years of history", "http://www.museums.norfolk.gov.uk/Visit_Us/Norwich_Castle/index.htm"]
+      ["norwichcastlead.svg", "Norwich Castle - Museum & Art Gallery - 900 years of history", "http://www.museums.norfolk.gov.uk/Visit_Us/Norwich_Castle/index.htm"]
     ];
 
     var adid = Math.floor(Math.random() * ads.length);
